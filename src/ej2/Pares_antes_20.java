7d4c0582b7262b26a5fa209ee1f578a4afe07769
Maxim <74974283+maximka76667@users.noreply.github.com>
2021-10-14 14:23:23 +0200

    pares_antes_20

---
+++ src/ej2/Pares_antes_20.java
@@ -0,0 +1,11 @@
+package ej2;
+
+import java.util.Scanner;
+
+public class Pares_antes_20 {
+
+	public static void main(String[] args) {
+		for(int i = 0; i <= 20; i++) if(i % 2 == 0) System.out.println(i);
+	}
+
+}
