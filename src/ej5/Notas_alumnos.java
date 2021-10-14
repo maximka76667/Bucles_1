c2b1a5e12a43064844f7f3eb1599332ff816e6dc
Maxim <74974283+maximka76667@users.noreply.github.com>
2021-10-14 14:59:55 +0200

    notas_alumnos

---
+++ src/ej5/Notas_alumnos.java
@@ -0,0 +1,61 @@
+package ej5;
+
+import java.util.Scanner;
+
+public class Notas_alumnos {
+
+	public static void main(String[] args) {
+		int numero_alumno = 0;
+		
+		Scanner entrada = new Scanner(System.in);
+		
+		System.out.print("Cantidad de alumnos: ");
+		int cantidad = entrada.nextInt();
+		
+		int[] notas;
+		notas = new int[cantidad];
+		
+		for(int i = 0; i < cantidad; i++) {
+			System.out.print("Nota: ");
+			int nota = entrada.nextInt();
+			notas[numero_alumno] = nota;
+			++numero_alumno;
+		}
+		
+		for(int i = 0; i < notas.length; i++) {
+			switch (notas[i]) {
+			case 0:
+			case 1:
+			case 2:
+			case 3:
+			case 4:
+				System.out.println("Alumno numero " + i + " - Insuficiente");
+				break;
+				
+			case 5:
+				System.out.println("Alumno numero " + i + " - Suficiente");
+				break;
+				
+			case 6:
+				System.out.println("Alumno numero " + i + " - Bien");
+				break;
+				
+			case 7:
+			case 8:
+				System.out.println("Alumno numero " + i + " - Notable");
+				break;
+				
+			case 9:
+			case 10:
+				System.out.println("Alumno numero " + i + " - Sobresaliente");
+				break;
+	
+			default:
+				break;
+			}
+		}
+		
+		entrada.close();
+	}
+
+}
