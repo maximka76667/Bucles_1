3b130b2b70e506c445a5aebb67f8c2568820f370
Maxim <74974283+maximka76667@users.noreply.github.com>
2021-10-14 14:26:13 +0200

    potencias_2

---
+++ src/ej3/Potencias_2.java
@@ -0,0 +1,7 @@
+package ej3;
+
+public class Potencias_2 {
+	public static void main(String[] args) {
+		for(int i = 0; i <= 10; i++) System.out.println("2 en potencia " + i + ": " + Math.pow(2, i));
+	}
+}
